1a19926ae2978251c070533fcd11f71a4a79a97c
gonzalouj <gonzalo.urra@alumnos.uach.cl>
2026-03-31 21:53:06 -0300

    chore: fix local docker run configuration

--- schema_unificado_new.sql
+++ schema_unificado_new.sql
@@ -180,7 +180,7 @@ INSERT INTO usuarios (
 ) VALUES (
     '12345678-9',
     'Administrador Sistema',
-    'admin@empresa.com',
+    'correo@empresa.com',
     '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', -- password: "password"
     'Admin',
     true
